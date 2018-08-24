1764e9ede337f9c3396ee49eb360c602f5b0f150
taoren7 <taoren_7@163.com>
2018-08-24 16:50:49 +0800

    delete movie test

--- microservice-provider-user/src/test/java/cn/allen/cloud/MicroserviceProviderUserApplicationTests.java
+++ microservice-provider-user/src/test/java/cn/allen/cloud/MicroserviceProviderUserApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MicroserviceSimpleProviderUserApplicationTests {
+public class MicroserviceProviderUserApplicationTests {
 
 	@Test
 	public void contextLoads() {

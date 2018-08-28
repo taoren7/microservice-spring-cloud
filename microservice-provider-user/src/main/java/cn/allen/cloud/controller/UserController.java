116ed623fdd85e42de6daab4f691dc76a046182a
taoren7 <taoren_7@163.com>
2018-08-28 18:09:21 +0800

    delete movie test

--- microservice-provider-user/src/main/java/cn/allen/cloud/controller/UserController.java
+++ microservice-provider-user/src/main/java/cn/allen/cloud/controller/UserController.java
@@ -2,7 +2,12 @@ package cn.allen.cloud.controller;
 
 import cn.allen.cloud.entity.User;
 import cn.allen.cloud.repository.UserRepository;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +18,29 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    @Qualifier("eurekaClient")
+    private EurekaClient eurekaClient;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/simple/{id}")
     //public User findById(@PathVariable("id") Long id) {
     public User findById(@PathVariable Long id) {
         return userRepository.findOne(id);
+    }
+
+    @GetMapping("/eureka-instance")
+    public String serviceUrl() {
+        InstanceInfo instance = this.eurekaClient.getNextServerFromEureka("MICROSERVICE-PROVIDER-USER", false);
+        return instance.getHomePageUrl();
+    }
+
+    @GetMapping("/instance-info")
+    public ServiceInstance showInfo() {
+        ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+        return localServiceInstance;
     }
 
 }
